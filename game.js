7fc581cdc0d4f9f578af13db06fc0be2ce568b8a
Darshan Parajuli <parajulidarshan@gmail.com>
2014-10-04 13:53:47 -0700

    update

--- game.js
+++ game.js
@@ -1,6 +1,16 @@
+function Cell(x, y, val) {
+  this.x = x;
+  this.y = y;
+  this.val = val;
+}
+
 function Grid(w, h, cellSize) {
+  this.consts = {"food", }
   this.row = Math.round(h / cellSize);
   this.col = (w / cellSize);
+  this.INVALID = 0;
+  this.VALID = 1;
+  this.FOOD = 2;
   this.grid = new Array(this.row);
   this.init = function() {
     for (var i = 0; i < this.grid.length; i++) {
@@ -9,7 +19,7 @@ function Grid(w, h, cellSize) {
 
     for (var i = 0; i < this.row; i++) {
       for (var j = 0; j < this.col; j++) {
-        
+        this.grid[i][j] = this.VALID 
       }
     }
   }
@@ -21,9 +31,10 @@ function Body(x, y) {
   this.y = y;
 }
 
-function Snake(init_x, init_y) {
+function Snake(init_x, init_y, w, h, cellSize) {
   this.x = init_x;
   this.y = init_y;
+  this.grid = new Grid(w, h, cellSize);
   this.arr = new Array();
   this.init = function() {
     
